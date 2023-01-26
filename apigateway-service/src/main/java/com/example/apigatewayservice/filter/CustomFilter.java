3d2815b56ccbaeaad3898b013378fc22a2e01fc6
Ssantoo <nowing0108@kakao.com>
2023-01-27 00:28:47 +0900

    feat: 필터등록

--- apigateway-service/src/main/java/com/example/apigatewayservice/filter/CustomFilter.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/CustomFilter.java
@@ -12,30 +12,26 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
 
-    public CustomFilter(){
+    public CustomFilter() {
         super(Config.class);
+    }
+
+    public static class Config{
+        //put the configuration info
     }
 
     @Override
     public GatewayFilter apply(Config config) {
-
-        //Custom Pre Filter
-        return (exchange, chain) -> {
+        return ((exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
 
-            log.info("Custom Pre filter: request id -> {}", request.getId());
+            log.info("Custom PRE Filter : request id = {}", request.getId());
 
-            //Custom Post Filter
+            //custom post filter
             return chain.filter(exchange).then(Mono.fromRunnable(()->{
-                log.info("Custom Post filter: response code -> {}", response.getStatusCode());
+                log.info("Custom POST Filter : response code = {}", response.getStatusCode());
             }));
-        };
+        });
     }
-
-    public static class Config{
-        //Put the configuration properties
-
-    }
-
 }
--- apigateway-service/src/main/java/com/example/apigatewayservice/filter/LoggingFilter.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/LoggingFilter.java
@@ -3,7 +3,9 @@ package com.example.apigatewayservice.filter;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -11,40 +13,42 @@ import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
-public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
 
-    public GlobalFilter(){
+    public LoggingFilter(){
         super(Config.class);
+    }
+
+    @Data
+    public static class Config{
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
     }
 
     @Override
     public GatewayFilter apply(Config config) {
 
-        return (exchange, chain) -> {
+        GatewayFilter filter = new OrderedGatewayFilter((exchange, chain)->{
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
 
-            log.info("Global Filter baseMessage: {}", config.getBaseMessage());
+            log.info("Logging Filter baseMessage : {}", config.getBaseMessage());
 
             if(config.isPreLogger()){
-                log.info("Global Filter Start: request id -> {}", request.getId());
+                log.info("Logging PRE Start : request id = {}", request.getId());
             }
 
-            //Custom Post Filter
             return chain.filter(exchange).then(Mono.fromRunnable(()->{
-                if(config.isPostLogger()) {
-                    log.info("Global Filter End: response code -> {}", response.getStatusCode());
+                if(config.isPostLogger()){
+                    log.info("Logging POST End : response code = {}", response.getStatusCode());
                 }
             }));
-        };
+        }, Ordered.LOWEST_PRECEDENCE); //우선순위
+
+        return filter;
     }
 
-    @Data
-    public static class Config{
-        //Put the configuration properties
-        private String baseMessage;
-        private boolean preLogger;
-        private boolean postLogger;
-    }
+
 
 }
