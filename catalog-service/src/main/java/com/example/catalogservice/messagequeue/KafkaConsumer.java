19eebc023b43f4f66a5bfa46d5ed6c86c2496dd8
Ssantoo <nowing0108@kakao.com>
2023-02-09 23:13:00 +0900

    feat: updateQty

--- catalog-service/src/main/java/com/example/catalogservice/messagequeue/KafkaConsumer.java
+++ catalog-service/src/main/java/com/example/catalogservice/messagequeue/KafkaConsumer.java
@@ -1,6 +1,9 @@
 package com.example.catalogservice.messagequeue;
 
+import com.example.catalogservice.jpa.CatalogEntity;
 import com.example.catalogservice.jpa.CatalogRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,9 +26,18 @@ public class KafkaConsumer {
 
         Map<Object, Object> map = new HashMap<>();
         ObjectMapper mapper = new ObjectMapper();
+        try{
+            map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {});
+        } catch (JsonProcessingException ex){
+            ex.printStackTrace();
+        }
 
-        
+        CatalogEntity entity = catalogRepository.findByProductId((String)map.get("productId"));
 
+        if(entity != null){
+            entity.setStock(entity.getStock() - (Integer)map.get("qty"));
+            catalogRepository.save(entity);
+        }
 
 
     }
